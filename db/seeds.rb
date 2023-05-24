7b0a8a71e32e4ca3d1a4ab17f8b771f498e24e80
Jamie Graham <jagraham04@yahoo.com>
2023-05-24 16:46:35 +0100

    Created Yelp clone using rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants = [
+  { name: "Dishoom", address: "7 Boundary St, Covent Garden", category: "chinese" },
+  { name: "Pizza East", address: "56A Shoreditch High St, Shoreditch", category: "italian" },
+  { name: "Momo House", address: "13 London Road, Dalston", category: "belgian" },
+  { name: "Jerk Chicken", address: "2 Jerk Street, Hoxton", category: "french" },
+  { name: "Italian Food", address: "10 Italy Way, Little Italy", category: "italian" }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
